d9569d82549482f63c90ed204b6a7fc84c5ec4b0
Sagit Gutierrez <jozaguts@gmail.com>
2022-06-06 11:08:29 -0500

    show buttons "aceptar", "cancelar" on update

--- src/components/Exams/Form.vue
+++ src/components/Exams/Form.vue
@@ -1,135 +1,147 @@
 <template>
   <ValidationObserver v-slot="{ invalid }" ref="store-exam-form">
     <v-form ref="form">
-    <v-container>
-      <v-row>
-        <v-col>
-          <h5 class="text-h5">Redactar Examen</h5>
-          <small class="text-secondary">*Todos los campos son requeridos</small>
-        </v-col>
-      </v-row>
-      <v-row>
-        <v-col cols="4">
-          <validation-provider
+      <v-container>
+        <v-row v-if="$store.state.examen.dialogUpdate">
+          <v-col class="d-flex justify-end">
+            <CancelButton @reset-form="$refs['store-exam-form'].reset()" />
+            <!--          todo refactor saved-->
+            <SaveButton :invalid="invalid" @saved="saved" />
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col>
+            <h5 class="text-h5">Redactar Examen</h5>
+            <small class="text-secondary"
+              >*Todos los campos son requeridos</small
+            >
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col cols="4">
+            <validation-provider
               v-slot="{ errors }"
               name="Nombre"
-              :rules="{required: true}"
-          >
-            <v-text-field
+              :rules="{ required: true }"
+            >
+              <v-text-field
                 label="Titulo del examen"
                 v-model="name"
                 :error-messages="errors"
-            ></v-text-field>
-          </validation-provider>
-        </v-col>
-        <v-col cols="4">
-          <validation-provider
+              ></v-text-field>
+            </validation-provider>
+          </v-col>
+          <v-col cols="4">
+            <validation-provider
               v-slot="{ errors }"
               name="Materia"
-              :rules="{required: true}"
-          >
-          <v-select
-              label="Materia"
-              :items="subjects"
-              v-model="subject_id"
-              :error-messages="errors"
-          />
-          </validation-provider>
-        </v-col>
-        <v-col cols="4">
-          <validation-provider
+              :rules="{ required: true }"
+            >
+              <v-select
+                label="Materia"
+                :items="subjects"
+                v-model="subject_id"
+                :error-messages="errors"
+              />
+            </validation-provider>
+          </v-col>
+          <v-col cols="4">
+            <validation-provider
               v-slot="{ errors }"
               name="Profesor"
-              :rules="{required: true}"
-          >
-          <v-select
-              label="Profesor"
-              :items="users"
-              :error-messages="errors"
-              v-model="user_id"/>
-          </validation-provider>
-        </v-col>
-      </v-row>
-      <v-row>
-        <v-col cols="12">
-          <p class="text-caption">¿Numero de preguntas por nivel?</p>
-        </v-col>
-        <v-col cols="12" md="2" lg="2">
-          <validation-provider
+              :rules="{ required: true }"
+            >
+              <v-select
+                label="Profesor"
+                :items="users"
+                :error-messages="errors"
+                v-model="user_id"
+              />
+            </validation-provider>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col cols="12">
+            <p class="text-caption">¿Numero de preguntas por nivel?</p>
+          </v-col>
+          <v-col cols="12" md="2" lg="2">
+            <validation-provider
               v-slot="{ errors }"
               name="Nivel bajo"
-              :rules="{required: true}"
-          >
-          <v-text-field
-              :error-messages="errors"
-              outlined
-              dense
-              type="number"
-              label="Nivel bajo"
-              v-model.number="low"
-          ></v-text-field>
-          </validation-provider>
-        </v-col>
-        <v-col cols="12" md="2" lg="2">
-          <validation-provider
+              :rules="{ required: true }"
+            >
+              <v-text-field
+                :error-messages="errors"
+                outlined
+                dense
+                type="number"
+                label="Nivel bajo"
+                v-model.number="low"
+              ></v-text-field>
+            </validation-provider>
+          </v-col>
+          <v-col cols="12" md="2" lg="2">
+            <validation-provider
               v-slot="{ errors }"
               name="Nivel medio"
-              :rules="{required: true}"
-          >
-          <v-text-field
-              outlined
-              :error-messages="errors"
-              dense
-              type="number"
-              label="Nivel medio"
-              v-model.number="medium"
-          ></v-text-field>
-          </validation-provider>
-        </v-col>
-        <v-col cols="12" md="2" lg="2">
-          <validation-provider
+              :rules="{ required: true }"
+            >
+              <v-text-field
+                outlined
+                :error-messages="errors"
+                dense
+                type="number"
+                label="Nivel medio"
+                v-model.number="medium"
+              ></v-text-field>
+            </validation-provider>
+          </v-col>
+          <v-col cols="12" md="2" lg="2">
+            <validation-provider
               v-slot="{ errors }"
               name="Nivel alto"
-              :rules="{required: true}"
-          >
-
-          <v-text-field
-              outlined
-              :error-messages="errors"
-              dense
-              type="number"
-              label="Nivel alto"
-              v-model.number="high"
-          ></v-text-field>
-          </validation-provider>
-        </v-col>
-        <v-col cols="12" class="py-0">
-          <small
+              :rules="{ required: true }"
+            >
+              <v-text-field
+                outlined
+                :error-messages="errors"
+                dense
+                type="number"
+                label="Nivel alto"
+                v-model.number="high"
+              ></v-text-field>
+            </validation-provider>
+          </v-col>
+          <v-col cols="12" class="py-0">
+            <small
               v-if="$store.getters['examen/totalQuestions']"
-              class="text-caption">Total de preguntas: {{$store.getters['examen/totalQuestions']}}</small>
-        </v-col>
-      </v-row>
-      <questions-component
-        ref="questionComponent"
-        v-for="question in questions"
-        :options="question.options"
-        :key="question.name"
-        :question="question"
-      />
-      <v-row>
-        <v-col cols="12" class="d-flex justify-end">
-          <v-btn color="secondary" tile class="mx-3" @click="addRow"
-            >Agregar pregunta</v-btn
-          >
-        </v-col>
-        <v-col class="d-flex justify-end">
-          <CancelButton @reset-form="$refs['store-exam-form'].reset()"/>
-<!--          todo refactor saved-->
-          <SaveButton :invalid="invalid" @saved="saved"/>
-        </v-col>
-      </v-row>
-    </v-container>
-  </v-form>
+              class="text-caption"
+              >Total de preguntas:
+              {{ $store.getters["examen/totalQuestions"] }}</small
+            >
+          </v-col>
+        </v-row>
+        <questions-component
+          ref="questionComponent"
+          v-for="question in questions"
+          :options="question.options"
+          :key="question.name"
+          :question="question"
+        />
+        <v-row>
+          <v-col cols="12" class="d-flex justify-end">
+            <v-btn color="secondary" tile class="mx-3" @click="addRow"
+              >Agregar pregunta</v-btn
+            >
+          </v-col>
+          <v-col class="d-flex justify-end">
+            <CancelButton @reset-form="$refs['store-exam-form'].reset()" />
+            <!--          todo refactor saved-->
+            <SaveButton :invalid="invalid" @saved="saved" />
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-form>
   </ValidationObserver>
 </template>
 
@@ -178,24 +190,24 @@ export default {
         return this.$store.state.examen.editedItem.low;
       },
       set(value) {
-        this.$store.commit("examen/SET_LOW", value)
-      }
+        this.$store.commit("examen/SET_LOW", value);
+      },
     },
     medium: {
       get() {
         return this.$store.state.examen.editedItem.medium;
       },
       set(value) {
-        this.$store.commit("examen/SET_MEDIUM",value)
-      }
+        this.$store.commit("examen/SET_MEDIUM", value);
+      },
     },
     high: {
       get() {
         return this.$store.state.examen.editedItem.high;
       },
       set(value) {
-        this.$store.commit("examen/SET_HIGH",value)
-      }
+        this.$store.commit("examen/SET_HIGH", value);
+      },
     },
     questions() {
       return this.$store.state.examen.editedItem.questions;
@@ -203,15 +215,17 @@ export default {
   },
   methods: {
     saved() {
-      this.$refs['store-exam-form'].validate().then(success => {
-        if(success) {
-          this.$emit('saved')
+      this.$refs["store-exam-form"].validate().then((success) => {
+        if (success) {
+          this.$emit("saved");
+          this.reset();
         }
-      })
+      });
     },
-    reset(){
+    reset() {
       this.e1 = 1;
       this.$refs.form.reset();
+      this.$refs["store-exam-form"].reset();
     },
     addRow() {
       this.id += 1;
--- src/components/Exams/UpdateButton.vue
+++ src/components/Exams/UpdateButton.vue
@@ -20,45 +20,45 @@ export default {
     show(id) {
       this.loading = !this.loading;
       ExamenesService.get(id)
-          .then(({ data }) => {
-            data.data.questions.map(question => {
-              question.options.map((option,index) => {
-                if (index === 0 || index === 1){
-                  option.cols = "12";
-                  option.md = "4";
-                } else{
-                  option.cols = "10";
-                  option.md = "3";
-                }
-              })
-            })
-            this.$store.commit(
-                "examen/SET_EXAMEN_QUESTIONS",
-                data.data.questions
-            );
-            return data
-          })
-          .then(({ data }) => {
-            this.$nextTick(()=>{
-              this.$store.commit("examen/UPDATE_NAME", data.name);
-              this.$store.commit("examen/UPDATE_EXAMEN_ID", data.examen_id);
-              this.$store.commit("examen/UPDATE_SUBJECT_ID", data.subject_id);
-              this.$store.commit("examen/UPDATE_USER_ID", data.user_id);
-              this.$store.commit("examen/SET_LOW", data.low);
-              this.$store.commit("examen/SET_MEDIUM", data.medium);
-              this.$store.commit("examen/SET_HIGH", data.high);
-            })
-          })
-          .then(() => {
-            this.$nextTick(()=>{
-              this.loading = !this.loading;
-            })
-          })
-          .then(() => {
-            this.$nextTick(()=>{
-              this.$store.commit("examen/OPEN_UPDATE_DIALOG")
-            })
+        .then(({ data }) => {
+          data.data.questions.map((question) => {
+            question.options.map((option, index) => {
+              if (index === 0 || index === 1) {
+                option.cols = "12";
+                option.md = "4";
+              } else {
+                option.cols = "10";
+                option.md = "3";
+              }
+            });
           });
+          this.$store.commit(
+            "examen/SET_EXAMEN_QUESTIONS",
+            data.data.questions
+          );
+          return data;
+        })
+        .then(({ data }) => {
+          this.$nextTick(() => {
+            this.$store.commit("examen/UPDATE_NAME", data.name);
+            this.$store.commit("examen/UPDATE_EXAMEN_ID", data.examen_id);
+            this.$store.commit("examen/UPDATE_SUBJECT_ID", data.subject_id);
+            this.$store.commit("examen/UPDATE_USER_ID", data.user_id);
+            this.$store.commit("examen/SET_LOW", data.low);
+            this.$store.commit("examen/SET_MEDIUM", data.medium);
+            this.$store.commit("examen/SET_HIGH", data.high);
+          });
+        })
+        .then(() => {
+          this.$nextTick(() => {
+            this.loading = !this.loading;
+          });
+        })
+        .then(() => {
+          this.$nextTick(() => {
+            this.$store.commit("examen/OPEN_UPDATE_DIALOG");
+          });
+        });
     },
   },
 };
