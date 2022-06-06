ab9246073cf20f85427a511784a30386fb7bc0ef
Sagit Gutierrez <jozaguts@gmail.com>
2022-06-06 02:19:42 -0500

    update-create dialog

--- src/components/shared/Dialog.vue
+++ src/components/shared/Dialog.vue
@@ -1,17 +1,20 @@
 <template>
   <v-dialog :value="$store.state.settings.dialog" max-width="510px">
     <v-card>
-      <v-card-title class="title grey--text text--darken-1">{{message}}</v-card-title>
+      <v-card-title class="title grey--text text--darken-1">{{
+        message
+      }}</v-card-title>
       <v-card-actions>
         <v-spacer></v-spacer>
-        <v-btn color="primary" outlined text @click="$store.commit('settings/TOGGLE_DIALOG',false)"
-        >Cancelar</v-btn
-        >
         <v-btn
-            color="primary"
-            @click="$emit('store')"
-            :loading="loading"
-        >Aceptar</v-btn
+          color="primary"
+          outlined
+          text
+          @click="$store.commit('settings/TOGGLE_DIALOG', false)"
+          >Cancelar</v-btn
+        >
+        <v-btn color="primary" @click="$emit('store')" :loading="loading"
+          >Aceptar</v-btn
         >
       </v-card-actions>
     </v-card>
@@ -21,13 +24,11 @@
 <script>
 export default {
   name: "Dialog",
-  props: ['message','loading'],
+  props: ["message", "loading"],
   data() {
-    return {}
+    return {};
   },
-}
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
