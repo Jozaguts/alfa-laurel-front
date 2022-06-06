54d43eb1e8ab786ddcdfd54a2351b163fe0ad460
Sagit Gutierrez <jozaguts@gmail.com>
2022-06-06 02:19:00 -0500

    chips: roles and permission component

---
+++ src/components/Users/RolesPermissionsCard.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-card v-if="!!roleSelected" tile flat>
+    <v-card-text>
+      <small>Permisos:</small>
+      <v-chip-group active-class="primary--text" column>
+        <v-chip v-for="permissionRole in permissionsRole" :key="permissionRole" small>
+          {{ permissionRole }}
+        </v-chip>
+      </v-chip-group>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "roles-permissions-card",
+  props: {
+    roles:{
+      type: Array,
+      default: ()=> [],
+      required: true,
+    },
+    roleSelected: {
+      type: String,
+      require: true,
+    },
+  },
+  computed: {
+    permissionsRole() {
+      let [permissionsRole] = this.roles
+        .filter((role) => role.value === this.roleSelected)
+        .map((role) => role.permissions.map((permission) => permission.name));
+      return permissionsRole || [];
+    },
+  },
+};
+</script>
+
+<style scoped></style>
