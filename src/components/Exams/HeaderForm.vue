3e57a32fd0c3bc92496391e6703b76c76c169105
Sagit Gutierrez <jozaguts@gmail.com>
2022-06-06 22:25:37 -0500

    Exam form: header

---
+++ src/components/Exams/HeaderForm.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-row>
+    <v-col>
+      <h5 class="text-h5">Redactar Examen</h5>
+      <small class="text-secondary"
+      >*Todos los campos son requeridos</small
+      >
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: "HeaderForm"
+};
+</script>
+
+<style scoped>
+
+</style>
