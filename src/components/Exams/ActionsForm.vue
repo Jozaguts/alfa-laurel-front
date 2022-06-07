b4f1c048218485f09b9a18bc4a3a93c3b7f84fdb
Sagit Gutierrez <jozaguts@gmail.com>
2022-06-06 22:29:40 -0500

    exam form: actions component

---
+++ src/components/Exams/ActionsForm.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-row v-if="$store.state.examen.dialogUpdate || visibleForce">
+    <v-col class="d-flex justify-end">
+      <CancelButton @reset-form="$emit('reset-form')" />
+      <SaveButton :invalid="invalid" @saved="$emit('saved')" />
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: "ActionsForm",
+  props:['invalid','visibleForce'],
+  components: {
+    CancelButton: () => import("@/components/Exams/CancelButton.vue"),
+    SaveButton: () => import("@/components/Exams/SaveButton.vue"),
+  },
+};
+</script>
